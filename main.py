bc7b04c1bef50c1645f85a4b72f1641bdaf6bc9b
F N <fernando@icarusdev.com.ar>
2012-06-16 19:36:08 -0300

    FN:ALL: portfolio

--- main.py
+++ main.py
@@ -19,34 +19,107 @@ class News(db.Model):
 class MainPage(webapp.RequestHandler):
     def get(self):        
         news_for_index = db.GqlQuery("SELECT * FROM News")
-        template_values = {'lista_de_news': news_for_index}
+        
+        list_of_files = []
+        for root, dirs, files in os.walk('images/portfolio/'):
+            for name in files:
+                full_name = os.path.join(root, name)
+                list_of_files.append(full_name)
+                logging.info("ROOT: %s, NAME: %s, DIRS:%s" % (root, name, dirs))
+        
+        #TEMPLTES VALUES FOR THE INDEX
+        template_values = {
+        'lista_de_news': news_for_index,
+        'list_of_portfolio': list_of_files
+        }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path,template_values))
 
 class AboutPage(webapp.RequestHandler):
     def get(self):
+                
+        list_of_files = []
+        for root, dirs, files in os.walk('images/portfolio/'):
+            for name in files:
+                full_name = os.path.join(root, name)
+                list_of_files.append(full_name)
+                logging.info("ROOT: %s, NAME: %s, DIRS:%s" % (root, name, dirs))
+        
+        #TEMPLTES VALUES FOR THE INDEX
+        template_values = {
+        'list_of_portfolio': list_of_files
+        }
         path = os.path.join(os.path.dirname(__file__), 'about.html')
-        self.response.out.write(template.render(path,None))
+        self.response.out.write(template.render(path,template_values))
 
 class PrivacyPage(webapp.RequestHandler):
     def get(self):
+                
+        list_of_files = []
+        for root, dirs, files in os.walk('images/portfolio/'):
+            for name in files:
+                full_name = os.path.join(root, name)
+                list_of_files.append(full_name)
+                logging.info("ROOT: %s, NAME: %s, DIRS:%s" % (root, name, dirs))
+        
+        #TEMPLTES VALUES FOR THE INDEX
+        template_values = {
+        'list_of_portfolio': list_of_files
+        }
         path = os.path.join(os.path.dirname(__file__), 'privacy.html')
-        self.response.out.write(template.render(path,None))
+        self.response.out.write(template.render(path,template_values))
 
 class GalleryPage(webapp.RequestHandler):
     def get(self):
+                
+        list_of_files = []
+        for root, dirs, files in os.walk('images/portfolio/'):
+            for name in files:
+                full_name = os.path.join(root, name)
+                list_of_files.append(full_name)
+                logging.info("ROOT: %s, NAME: %s, DIRS:%s" % (root, name, dirs))
+        
+        #TEMPLTES VALUES FOR THE INDEX
+        template_values = {
+        'list_of_portfolio': list_of_files
+        }
         path = os.path.join(os.path.dirname(__file__), 'gallery.html')
-        self.response.out.write(template.render(path,None))
+        self.response.out.write(template.render(path,template_values))
         
 class SitemapPage(webapp.RequestHandler):
     def get(self):
+        
+        list_of_files = []
+        for root, dirs, files in os.walk('images/portfolio/'):
+            for name in files:
+                full_name = os.path.join(root, name)
+                list_of_files.append(full_name)
+                logging.info("ROOT: %s, NAME: %s, DIRS:%s" % (root, name, dirs))
+        
+        #TEMPLTES VALUES FOR THE INDEX
+        template_values = {
+        'list_of_portfolio': list_of_files
+        }
         path = os.path.join(os.path.dirname(__file__), 'sitemap.html')
-        self.response.out.write(template.render(path,None))        
+        self.response.out.write(template.render(path,template_values))        
 
 class ContactsPage(webapp.RequestHandler):
     def get(self):
+        
+        list_of_files = []
+        for root, dirs, files in os.walk('images/portfolio/'):
+            for name in files:
+                full_name = os.path.join(root, name)
+                list_of_files.append(full_name)
+                logging.info("ROOT: %s, NAME: %s, DIRS:%s" % (root, name, dirs))
+        
+        #TEMPLTES VALUES FOR THE INDEX
+        template_values = {
+        'list_of_portfolio': list_of_files
+        }
         path = os.path.join(os.path.dirname(__file__), 'contacts.html')
-        self.response.out.write(template.render(path,None))
+        self.response.out.write(template.render(path,template_values))
+        
     def post(self):
         sender = 'fernando@icarusdev.com.ar'        
         subject = 'Contact from website'
@@ -57,6 +130,7 @@ class ContactsPage(webapp.RequestHandler):
         message.reply_to = self.request.get('email')
         message.send()
         self.redirect('/contacts')
+
 
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
